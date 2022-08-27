bff2ea199210072727320c82eb1a1d6cec0e1f8b
kadu <kadu.silva2014@gmail.br>
2022-08-27 11:10:30 -0300

    feat:❇ Adicona url no bando de dados

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 // src/app.js
 
 const express = require('express');
+const { insertURL } = require('./utils/DB');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).redirect('/admin'));
+app.post('/add-url', async (req, res) => {
+  const { body } = req;
+  console.log('body', body);
 
-app.post('/add' )
+  const result = await insertURL(body);
 
-module.exports = app
+  res.status(201).json(result);
+});
+
+module.exports = app;
---
+++ src/utils/DB.js
@@ -0,0 +1,47 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+const createURL = require('./urlGenerator');
+
+const path = '../files/DB.json';
+const readFile = async () => {
+    try {
+        const completePath = join(__dirname, path);
+        const contentFile = await fs.readFile(completePath, 'utf-8');
+        return JSON.parse(contentFile);
+    } catch (e) {
+        console.error('Erro ao abrir o arquivo', e.message);
+        return null;
+    }
+};
+
+const writeFile = async (content) => {
+    try {
+        const completePath = join(__dirname, path);
+        await fs.writeFile(completePath, JSON.stringify(content, null, 2));
+    } catch (e) {
+        console.error('Erro ao salvar o arquivo', e.message);
+        return null;
+    }
+};
+
+const insertURL = async ({ url }) => {
+  const data = await readFile();
+  const newShortURL = {
+    id: data.nextID,
+    shortURL: createURL(),
+    originalURL: url,
+  };
+
+  data.urls.push(newShortURL);
+  data.nextID += 1;
+
+  await writeFile(data);
+
+  return newShortURL;
+};
+
+module.exports = {
+  readFile,
+  writeFile,
+  insertURL,
+};
