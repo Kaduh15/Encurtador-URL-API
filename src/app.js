4a5896594cc7a97aaf925edb7d88f0626f4c571a
kadu <kadu.silva2014@gmail.br>
2022-08-29 11:38:51 -0300

    refactor:♻  Separa routas em uma pasta

--- src/app.js
+++ src/app.js
@@ -2,27 +2,17 @@
 
 const express = require('express');
 
-const { insertURL, getOriginalURL } = require('./utils/DB');
+const main = require('./routes/main');
+const addURL = require('./routes/addURL');
 
 const app = express();
 app.use(express.json());
 
-app.post('/add-url', async (req, res) => {
-  const { body } = req;
+// Routes
+app.use('/', main);
+app.use('/add-url', addURL);
 
-  const result = await insertURL(body);
-
-  res.status(201).json(result);
-});
-
-app.get('/:shortURL', async (req, res) => {
-  const { shortURL } = req.params;
-
-  const urlOriginal = await getOriginalURL(shortURL);
-
-  res.status(200).redirect(urlOriginal);
-});
-
+// Middleware de Error
 app.use((err, req, res, _next) => {
   res
     .status(500)
---
+++ src/routes/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getOriginalURL } = require('../utils/DB');
+
+const router = express.Router();
+
+router.get('/:shortURL', async (req, res) => {
+  const { shortURL } = req.params;
+
+  const urlOriginal = await getOriginalURL(shortURL);
+
+  res.status(200).redirect(urlOriginal);
+});
+
+module.exports = router;
