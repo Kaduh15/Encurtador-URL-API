5a215b2084b603e8eff53c92e550621a0f890dce
kadu <kadu.silva2014@gmail.br>
2022-08-29 11:21:51 -0300

    refactor:♻ corrige o middleware de erro

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 // src/app.js
 
-const express = require("express");
+const express = require('express');
 
-const { insertURL, getOriginalURL } = require("./utils/DB");
+const { insertURL, getOriginalURL } = require('./utils/DB');
 
 const app = express();
 app.use(express.json());
 
-app.post("/add-url", async (req, res) => {
+app.post('/add-url', async (req, res) => {
   const { body } = req;
 
   const result = await insertURL(body);
@@ -15,18 +15,18 @@ app.post("/add-url", async (req, res) => {
   res.status(201).json(result);
 });
 
-app.get("/:shortURL", async (req, res) => {
+app.get('/:shortURL', async (req, res) => {
   const { shortURL } = req.params;
 
   const urlOriginal = await getOriginalURL(shortURL);
 
   res.status(200).redirect(urlOriginal);
-
 });
 
 app.use((err, req, res, _next) => {
   res
     .status(500)
     .json({ message: `Algo deu errado! Mensagem: ${err.message}` });
+  });
 
 module.exports = app;
