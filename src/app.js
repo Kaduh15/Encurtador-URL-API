dbacbe7d6b2cc312652ca5ac59bbadf0f2ce2e94
Restyled.io <commits@restyled.io>
2022-08-29 14:09:52 +0000

    Restyled by prettier

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 // src/app.js
 
-const express = require('express');
-const {insertURL} = require('./utils/DB');
+const express = require("express");
+const { insertURL } = require("./utils/DB");
 
 const app = express();
 app.use(express.json());
 
-app.post('/add-url', async (req, res) => {
-  const {body} = req;
+app.post("/add-url", async (req, res) => {
+  const { body } = req;
 
   const result = await insertURL(body);
 
@@ -15,7 +15,9 @@ app.post('/add-url', async (req, res) => {
 });
 
 app.use((err, req, res, _next) => {
-  res.status(500).json({message : `Algo deu errado! Mensagem: ${err.message}`});
+  res
+    .status(500)
+    .json({ message: `Algo deu errado! Mensagem: ${err.message}` });
 });
 
 module.exports = app;
