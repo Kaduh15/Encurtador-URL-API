1f986612a98c71a3623a765ab212b543b460a7fd
kadu <kadu.silva2014@gmail.br>
2022-08-29 10:50:41 -0300

    feat:❇ adiciona middleware de erro

--- src/app.js
+++ src/app.js
@@ -8,11 +8,14 @@ app.use(express.json());
 
 app.post('/add-url', async (req, res) => {
   const { body } = req;
-  console.log('body', body);
 
   const result = await insertURL(body);
 
   res.status(201).json(result);
+});
+
+app.use((err, req, res, _next) => {
+  res.status(500).json({ message: `Algo deu errado! Mensagem: ${err.message}` });
 });
 
 module.exports = app;
