ea6a9fffae62c6b4a45e6935f7260c1e969269f1
kadu <kadu.silva2014@gmail.br>
2022-11-11 08:58:07 -0300

    chore: remove console

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2/promise');
 
-console.log({
-  host: process.env.MYSQLHOST || 'database',
-  port: process.env.MYSQLPORT || 3306,
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'senha',
-  database: process.env.MYSQLDATABASE || 'URLs_database',
-})
-
 const connection = mysql.createPool({
   host: process.env.MYSQLHOST || 'database',
   port: process.env.MYSQLPORT || 3306,
